26b9ed5a149533861c55ba94d4e1105b7981cf8a
Vikas Rana <vikas.rana@cloudsmartz.net>
2016-08-08 18:44:59 +0530

    changes reverted

--- app/app.js
+++ app/app.js
@@ -10,5 +10,5 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
